fedc225d6e1ad34360368ad62bff08c248a2e80a
serr <sc7227484@gmail.com>
2025-08-20 14:54:23 +0300

    quick update

--- lua/plugins/status-bar.lua
+++ lua/plugins/status-bar.lua
@@ -56,7 +56,7 @@ return  {
                     }
                 },
                 lualine_b = {'branch', 'diff', 'diagnostics'},
-                lualine_c = {'filename'},
+                lualine_c = {},
                 lualine_x = {'filetype'},
                 lualine_y = {'progress'},
                 lualine_z = {'location'}
