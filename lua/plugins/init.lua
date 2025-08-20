f6c9fe87e69b603253e0099a8ae9800d7df5833f
serr <sc7227484@gmail.com>
2025-08-20 16:02:48 +0300

    quick update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,6 +14,6 @@ local lazypath = vim.fn.stdpath("config") .. "/deps/plugins/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
 local for_setup = {}
 for _, v in ipairs(plugins) do
-   for_setup[#for_setup+1] = require(v)
+    for_setup[#for_setup+1] = require(v)
 end
 require("lazy").setup(for_setup)
