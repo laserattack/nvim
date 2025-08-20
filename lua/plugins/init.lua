6e9f38fde28fba21d867faf2ad1e4d5ffbcebfee
serr <sc7227484@gmail.com>
2025-08-20 15:36:04 +0300

    quick update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,25 +1,19 @@
 -- Настройка менеджера плагинов
+local plugins = {
+    "plugins/nvim-tree",
+    "plugins/color-scheme",
+    "plugins/tree-sitter",
+    "plugins/cmp",
+    "plugins/lsp",
+    "plugins/status-bar",
+    "plugins/mini-surround",
+    "plugins/dashboard",
+}
 
 local lazypath = vim.fn.stdpath("config") .. "/deps/plugins/lazy.nvim"
 vim.opt.rtp:prepend(lazypath)
--- Подключаемые плагины
-local plugins = {
-    -- Дерево директорий
-    require("plugins/nvim-tree"),
-    -- Цветовая тема
-    require("plugins/color-scheme"),
-    -- Treesitter - умная подсветка для выбранных языков (с учетом AST)
-    require("plugins/tree-sitter"),
-    -- Auto complete - предлагает варианты дополнения
-    require("plugins/cmp"),
-    -- lsp
-    require("plugins/lsp"),
-    -- status bar
-    require("plugins/status-bar"),
-    -- Для установки парных символов вокруг выделенного текста
-    require("plugins/mini-surround"),
-    -- Окно при входе в nvim
-    require("plugins/dashboard"),
-}
-
-require("lazy").setup(plugins)
+local for_setup = {}
+for _, v in ipairs(plugins) do
+   for_setup[#for_setup+1] = require(v)
+end
+require("lazy").setup(for_setup)
