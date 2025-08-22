9951731b6a7601828b930b83372659bf5c520fba
serr <sc7227484@gmail.com>
2025-08-22 14:52:21 +0300

    quick update

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,15 +1,5 @@
 local headers = {
     {
-        [[                (`.             ]],
-        [[                 \ `.           ]],
-        [[                  )  `._..---._ ]],
-        [[\`.       __...---`         o  )]],
-        [[ \ `._,--'           ,    ___,' ]],
-        [[  ) ,-._          \  )   _,-'   ]],
-        [[ /,'    ``--.._____\/--''       ]],
-        [[                                ]],
-    },
-    {
         [[                                   __       ]],
         [[                               _.-~  )      ]],
         [[                    _..--~~~~,'   ,-/     _ ]],
@@ -29,21 +19,42 @@ local headers = {
 }
 
 return {
-    dir = '~/.config/nvim/deps/plugins/dashboard-nvim',
+    dir = CFGP..'/deps/plugins/dashboard-nvim',
     event = 'VimEnter',
     config = function()
+
+        -- Встраиваю свою кастомную тему в плаги
+        -- файл с именем theme и расширением .lua должен быть в директории lua/plugins/
+
+        local theme = "dashboard-custom-theme"
+
+        -- Подключается какая то кастомная тема
+        if theme ~= "hyper" and theme ~= "doom" then
+            local custom_theme_src_path = CFGP..'/lua/plugins/'..theme..'.lua'
+            -- Если есть файл с кастомной темой в lua/plugins то подключается он
+            if vim.fn.filereadable(custom_theme_src_path) == 1 then
+                local custom_theme_dst_path = CFGP..'/deps/plugins/dashboard-nvim/lua/dashboard/theme/'..theme..'.lua'
+                if vim.fn.filereadable(custom_theme_dst_path) == 0 then
+                    local source_content = vim.fn.readfile(custom_theme_src_path)
+                    vim.fn.writefile(source_content, custom_theme_dst_path)
+                end
+            -- Указанного файла с кастомной темой нет => подключается стандартная hyper
+            else
+                theme = "hyper"
+            end
+        end
+        --
+
         vim.api.nvim_set_hl(0, 'DashboardHeader', { fg = '#7fb4ca' })
-        vim.api.nvim_set_hl(0, 'DashboardMruIcon', { fg = '#7fb4ca' })
         require('dashboard').setup {
+            theme = theme,
             config = {
-                header = headers[2],
+                header = headers[1],
                 shortcut = {
                     { desc = "Wanna code?" }
                 },
                 mru = {
                     enable = true,
-                    label = "",
-                    icon = "  Recently Files:",
                 },
                 project = {
                     enable = false,
@@ -53,6 +64,6 @@ return {
         }
     end,
     dependencies = {
-        { dir = '~/.config/nvim/deps/plugins/nvim-web-devicons' }
+        { dir = CFGP..'/deps/plugins/nvim-web-devicons' }
     }
 }
